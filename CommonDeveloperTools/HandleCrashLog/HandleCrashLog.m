764682dbd39e1c79d207615a8ef43abd71778aad
frostfeng <frost@frostdeMacBook-Pro.local>
2015-05-21 15:52:17 +0800

    完成crash 提取程序

--- CommonDeveloperTools/HandleCrashLog/HandleCrashLog.m
+++ CommonDeveloperTools/HandleCrashLog/HandleCrashLog.m
@@ -46,35 +46,151 @@
     [self.indicator startAnimation:nil];
     
     
-    @autoreleasepool {
-        NSString *home = self.crashLogPath;
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         
-        NSString *crashString = [NSString stringWithContentsOfFile:home encoding:NSUTF8StringEncoding error:nil];
+        @autoreleasepool {
+            NSString *home = self.crashLogPath;
+            
+            NSString *path = [home stringByAppendingString:@"_folder"];
+            
         
-        NSArray *crashArrays = [crashString componentsSeparatedByString:@"\r\n"];
-        NSMutableArray *resultArray = [NSMutableArray array];
-        
-        for (NSString *aCrash in crashArrays)
-        {
-            if ([aCrash containsString:@"6043093012"])
+            //解析整个crash log日志,分成单条日志
+            NSArray *crashArrays = nil;
+ 
+            NSString *crashString = [NSString stringWithContentsOfFile:home encoding:NSUTF8StringEncoding error:nil];
+            crashArrays = [crashString componentsSeparatedByString:@"\n"];
+
+            
+            // 解析想要的版本的crash日志
+            NSArray *versions = @[@"460",@"461"];       //添加想要的版本号到此即可.如：470
+            NSMutableDictionary *resultDiction = [NSMutableDictionary dictionary];
+            for (NSString *version in versions)
             {
-                [resultArray addObject:resultArray];
+                [resultDiction setObject:[NSMutableArray array] forKey:version];
+            }
+
+            // 按版本分开
+            for (NSString *aCrash in crashArrays)
+            {
+                [resultDiction enumerateKeysAndObjectsUsingBlock:^(NSString * key, NSMutableArray * obj, BOOL *stop)
+                {
+                    NSString *fromValue = [NSString stringWithFormat:@"60%@93012",key];
+                    if ([aCrash containsString:fromValue])
+                    {
+                        NSString *resultString = [aCrash stringByReplacingOccurrencesOfString:@"\\n" withString:@"\n"];
+                        [obj addObject:resultString];
+                        *stop = YES;
+                    }
+                }];
+            }
+            
+            // 提起crash
+            [resultDiction enumerateKeysAndObjectsUsingBlock:^(NSString * key, NSMutableArray * obj, BOOL *stop)
+             {
+                 [self handleCrashLogs:obj forVersion:key withHomePath:path];
+             }];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+           
+            NSAlert *alert = [NSAlert alertWithMessageText:@"Complete" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"Complete"];
+            NSUInteger action = [alert runModal];
+        });
+    });
+}
+
+- (NSArray*)getFromValueArrayFromVersionArray:(NSArray*)versionArray
+{
+    NSMutableArray *fromValueArray = [NSMutableArray array];
+    for (NSString *version in versionArray)
+    {
+        [fromValueArray addObject:[NSString stringWithFormat:@"60%@93012",version]];
+    }
+    return fromValueArray;
+}
+
+- (void)handleCrashLogs:(NSMutableArray*)crashLogs forVersion:(NSString*)version withHomePath:(NSString*)homePath
+{
+    if ([crashLogs count] > 0 && [version length] > 0 && [homePath length] > 0)
+    {
+        // 保证路径存在
+        NSString *newPath = [homePath stringByAppendingPathComponent:version];
+        [[NSFileManager defaultManager] createDirectoryAtPath:newPath withIntermediateDirectories:YES attributes:nil error:nil];
+        
+        // 保存整体crash log
+        NSMutableArray *writeLogs = [NSMutableArray arrayWithArray:crashLogs];
+        NSString * totalCount = [NSString stringWithFormat:@"Version Number:%@.  Total Crash Count is:%ld",version,[writeLogs count]];
+        [writeLogs insertObject:totalCount atIndex:0];
+        
+        NSString *handledString = [writeLogs componentsJoinedByString:@"\n"];
+        NSString *newFilePath = [homePath stringByAppendingString:@"_460.txt"];
+        [handledString writeToFile:newFilePath  atomically:YES encoding:NSUTF8StringEncoding error:nil];
+        
+        [self saveCrashLogs:crashLogs forVersion:version withHomePath:newPath withSaveName:@"all"];
+        
+        // 分crash类型保存
+        NSMutableArray *resultSVP = [NSMutableArray array];
+        NSMutableArray *resultFeed = [NSMutableArray array];
+        NSMutableArray *resultSogou = [NSMutableArray array];
+        NSMutableArray *resultFixed = [NSMutableArray array];
+        for (NSString *aCrash in crashLogs)
+        {
+            if ([aCrash containsString:@"SVPPlayerController"] || [aCrash containsString:@"AVPlayerItem"])
+            {
+                [resultSVP addObject:aCrash];
+            }
+            else if ([aCrash containsString:@"BaseDataSource"] || [aCrash containsString:@"NewsBaseViewController"])
+            {
+                [resultFeed addObject:aCrash];
+            }
+            else if ([aCrash containsString:@"SogouInput"])
+            {
+                [resultSogou addObject:aCrash];
+            }
+            else if ([aCrash containsString:@"preloadSibling"] || [aCrash containsString:@"offlineDidGetAbstractsList"] || [aCrash containsString:@"Supported orientations"] || [aCrash containsString:@"contains NaN"] || [aCrash containsString:@"SNLMatchLive setTeamA"])
+            {
+                //已经解决
+                [resultFixed addObject:aCrash];
             }
         }
         
-        NSString *handleString = [resultArray componentsJoinedByString:@"\r\n"];
+        // 其它crash类型
+        NSMutableArray *resultOther = [NSMutableArray arrayWithArray:crashLogs];
+        [resultOther removeObjectsInArray:resultSVP];
+        [resultOther removeObjectsInArray:resultFeed];
+        [resultOther removeObjectsInArray:resultSogou];
+        [resultOther removeObjectsInArray:resultFixed];
         
-        NSString *newFile = [home stringByAppendingString:@"handle.txt"];
+        //其它
+        [self saveCrashLogs:resultOther forVersion:version withHomePath:newPath withSaveName:@"其他"];
         
-        [handleString writeToFile:newFile  atomically:YES encoding:4 error:nil];
+        //SVP
+        [self saveCrashLogs:resultSVP forVersion:version withHomePath:newPath withSaveName:@"视频"];
+        
+        //Feed
+        [self saveCrashLogs:resultFeed forVersion:version withHomePath:newPath withSaveName:@"feed"];
+        
+        //Sogou
+        [self saveCrashLogs:resultSogou forVersion:version withHomePath:newPath withSaveName:@"输入法"];
+        
+        //已解决
+        [self saveCrashLogs:resultFixed forVersion:version withHomePath:newPath withSaveName:@"已解决"];
     }
-    
+
+}
+
+- (void)saveCrashLogs:(NSMutableArray*)crashLogs forVersion:(NSString*)version withHomePath:(NSString*)homePath withSaveName:(NSString*)name
+{
+    if ([crashLogs count] > 0 && [version length] > 0 && [homePath length] > 0)
+    {
+        NSMutableArray *writeLogs = [NSMutableArray arrayWithArray:crashLogs];
+        NSString * totalCount = [NSString stringWithFormat:@"Version Number:%@.  Total Crash Count is:%ld",version,[writeLogs count]];
+        [writeLogs insertObject:totalCount atIndex:0];
         
-    
-    [self.indicator startAnimation:nil];
-    self.indicator.hidden = YES;
-    
-    
+        NSString *handledString = [writeLogs componentsJoinedByString:@"\n"];
+        NSString *newFilePath = [homePath stringByAppendingFormat:@"/%@.txt",name];
+        [handledString writeToFile:newFilePath  atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    }
 }
 
 @end
